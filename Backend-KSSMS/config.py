4726db15727202e1a8c78f96af4bbc34c8f51104
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2025-07-17 09:47:54 +0530

    config.py added for temporary (Note: not valid for production)

---
+++ Backend-KSSMS/config.py
@@ -0,0 +1,13 @@
+params = {
+    "local_server": True,
+    "gmail": "chapadiyahansil@gmail.com",
+    "mail_password": "jqfkibxyimemrysu",
+    "API_KEY": "1234567890",
+    "SECRET_KEY": "1234567890",
+    "host": "localhost",
+    "port": 27017,
+    "username": "chapadiyahansil",
+    "password": "hansil@714",
+    "authentication_source": "admin"
+}
+
